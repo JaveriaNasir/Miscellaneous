e68f7f35a8f85c5b3b4a6121595e4fe6b316f214
Javeria Nasir <javeriamhdnsr@gmail.com>
2019-08-02 11:01:27 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PlaidInterview
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			bool finalResult = false;
+			Routing routingobj = new Routing();				
+			string strroutingnum = "111000025"; // valid  routing number
+			//string strroutingnum = "111ABCD25";	 // Invalid routing number		
+			if (!strroutingnum.All(char.IsDigit)) //checks for any non-digit value
+			{
+				finalResult = false;
+			}
+			else if (string.IsNullOrEmpty(strroutingnum)) //check for the empty values
+			{
+				finalResult = false;
+			}
+			else
+			{
+				int num1;
+				int.TryParse(strroutingnum, out num1);
+				int[] intarray = routingobj.GetIntArray(num1); //this method converts the string to an intArray
+				finalResult = routingobj.routingCheck(intarray); //this method acutally check for the right routing number if Modulus == 0
+			}
+			Console.WriteLine("The rountingcheck returned{0} " + finalResult);
+			Console.ReadLine();
+		}
+	}
+}
---
+++ Routing.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaidInterview
+{
+	class Routing
+	{
+		public bool routingCheck(int[] intarray)
+		{			
+			bool result = false;			
+			if (intarray.Length < 9)
+			{
+				result = false;
+			}
+			int mod = 3 * (intarray[0] + intarray[3] + intarray[6]) + 7 * (intarray[1] + intarray[4] + intarray[7]) + (intarray[2] + intarray[5] + intarray[8]);
+			if (mod % 10 == 0)
+			{
+				result = true;
+			}			
+			return result;
+		}
+
+		public int[] GetIntArray(int num)
+		{
+			List<int> listOfInts = new List<int>();
+			while (num > 0)
+			{
+				listOfInts.Add(num % 10);
+				num = num / 10;
+			}
+			listOfInts.Reverse();
+			return listOfInts.ToArray();
+		}
+	}
+}
